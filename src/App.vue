25af95d9ab060c4f54130b80f94658753cc1666f
user.name <1165893368@qq.com>
2020-04-22 18:07:53 +0800

    vue3.0初探

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <setUp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import setUp from './components/setUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    setUp
   }
 }
 </script>
---
+++ src/components/setUp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <p>{{data.count}}</p>
+    <button @click="addCount">++</button>
+
+  </div>
+</template>
+
+<script>
+  import { reactive } from 'vue'
+export default {
+  name: 'setUp',
+  props: {
+    msg: String
+  },
+  setup(){//替代了beforeCreate和create  在这两个生命周期函数之前调用
+    const data = reactive({ //响应式数据
+      count: 0
+    })
+    function addCount() {
+      data.count++
+    }
+    // return { 如果使用这样的方式那么将会失去响应式
+    //   ...data
+    // }
+    return {
+      data,
+      addCount
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
